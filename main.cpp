db694addb454f88c779fcbf0df6ffcbe7094aec9
SonOfAthena1 <soccerlc0095@gmail.com>
2025-12-02 16:24:18 -0800

    Setting up the program structure

--- main.cpp
+++ main.cpp
@@ -117,9 +117,20 @@ void printPath(pair<int,int> exitcell,
 // STUDENTS IMPLEMENT DFS HERE
 // Add arguments, return type, and logic
 // ----------------------------------------------------------
-// bool dfs(……) {
-//     // Your code here
-// }
+bool dfs(
+    int ent_r,
+    int ent_c,
+    vector<vector<int>>& maze,
+    const vector<vector<bool>>& visited,
+    const vector<vector<int>>& parent_r,
+    const vector<vector<int>>& parent_c,
+    int exit_r,
+    int exit_c
+) {
+
+
+    return false;
+}
 
 
 // ----------------------------------------------------------
@@ -159,17 +170,17 @@ int main() {
     // STUDENT WORK:
     // Call your DFS, track visited, and fill parent_r and parent_c
     // ------------------------------------------------------
-    // bool found = dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c);
+    bool found = dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c);
 
     // ------------------------------------------------------
     // STUDENT WORK:
     // If found, print the path
     // ------------------------------------------------------
-    // if (found) {
-    //     printPath(exitcell, parent_r, parent_c, ent_r, ent_c);
-    // } else {
-    //     cout << "\nNo path exists.\n";
-    // }
+    if (found) {
+        printPath(exitcell, parent_r, parent_c, ent_r, ent_c);
+    } else {
+        cout << "\nNo path exists.\n";
+    }
 
     return 0;
 }
